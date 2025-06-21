468bc01b86bcdf6ca861f1ba384269af6e6cb2bb
Miguel Migliorelli Bringhenti <miglenti@gmail.com>
2025-06-21 15:46:42 -0300

    fix: remove carrinho do middleware de autenticacao

--- routes/carrinho.php
+++ routes/carrinho.php
@@ -3,6 +3,4 @@
 use App\Http\Controllers\Carrinho\CarrinhoController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth')->group(function () {
-    Route::get('carrinho', [CarrinhoController::class, 'mostrar'])->name('carrinho');
-});
+Route::get('carrinho', [CarrinhoController::class, 'mostrar'])->name('carrinho');
