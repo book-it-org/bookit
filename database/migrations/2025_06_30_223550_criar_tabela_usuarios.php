e696c8a94304a8eadf4a483d7de4366603747982
MiguelForcaMariani <miguelforcamariani@gmail.com>
2025-06-30 22:04:21 -0300

    feat:droptable usuario

--- database/migrations/2025_06_30_223550_criar_tabela_usuarios.php
+++ database/migrations/2025_06_30_223550_criar_tabela_usuarios.php
@@ -48,6 +48,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('usuarios');
+        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('sessions');
     }
 };
