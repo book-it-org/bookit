b8b157a6740053b62b6a06a969b8db597286aae7
Miguel Migliorelli Bringhenti <miglenti@gmail.com>
2025-09-18 11:21:42 -0300

    feat: controller de funcionalidade de mostrar oferta pelo id, e mostrar titulo

--- app/Http/Controllers/Ofertas/OfertaController.php
+++ app/Http/Controllers/Ofertas/OfertaController.php
@@ -3,21 +3,39 @@
 namespace App\Http\Controllers\Ofertas;
 
 use App\Http\Controllers\Controller;
+use App\Models\Generos;
+use App\Models\Ofertas;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Redirect;
 use Inertia\Inertia;
 use Inertia\Response;
-use App\Models\Generos;
 
 class OfertaController extends Controller
 {
     /**
      * Renderiza a pagina de anunciar um livro
      */
-    public function mostrar(string $id): Response
+    public function mostrar(int $id, ?string $titulo_oferta = null): Response|RedirectResponse
     {
+        $oferta = Ofertas::where('id', $id)->with(['generos', 'idioma', 'usuario'])->first();
+
+        if (!$oferta) {
+            abort(404, 'Oferta não encontrada.');
+        }
+
+        $titulo_slug = $oferta->tituloParaSlug();
+        if ($titulo_oferta !== $titulo_slug) {
+            return Redirect::route('oferta', [
+                'id' => $id,
+                'titulo_oferta' => $titulo_slug,
+            ]);
+        }
+
         $generos = Generos::all();
+
         return Inertia::render('oferta/Oferta', [
-            'id_oferta' => $id,
-            'generos' => $generos
+            'oferta' => $oferta,
+            'generos' => $generos,
         ]);
     }
 }
