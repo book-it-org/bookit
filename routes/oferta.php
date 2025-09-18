3094fddf02189ac56b1f9dfa7b9e707f47df166b
Miguel Migliorelli Bringhenti <miglenti@gmail.com>
2025-09-18 11:21:08 -0300

    feat: adicao de parametro adiciona titulo_oferta na rota de oferta

--- routes/oferta.php
+++ routes/oferta.php
@@ -4,6 +4,6 @@ use App\Http\Controllers\Ofertas\OfertaController;
 use App\Http\Controllers\Ofertas\CriarOfertaController;
 use Illuminate\Support\Facades\Route;
 
-Route::get("oferta/{id}", [OfertaController::class, 'mostrar'])->name('oferta');
+Route::get("oferta/{id}/{titulo_oferta?}", [OfertaController::class, 'mostrar'])->name('oferta');
 Route::post("oferta", [CriarOfertaController::class, 'criarOferta'])->name('oferta.criar');
 
